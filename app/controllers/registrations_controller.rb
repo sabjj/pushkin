b15d9d60c6c9a91b404e49664b3afe214d3c3725
sabjj <lol.ru.sabj@gmail.com>
2015-03-25 15:57:20 +0300

    k

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,25 +6,14 @@ class RegistrationsController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
 	def create
-		#puts 222222222222222222222222222
-		#params.delete(:authenticity_token)
-  		#render plain: params.inspect
-  		#@questions = "%WORD% солнце знойное сушило,"
-  		
+
   		@questions = params[:question]
 		@token = params[:token]
 		preparation(@questions.to_s)
 		open_file
 		@ans = search_word
-  		render json: {answer: 'мглою'}
-  		#render json: {answer: @ans.to_s}
-		#puts @question
-		#preparation(@questions.to_s)
-		#open_file
-		#@ans = search_word
-		#puts @ans
-		#puts @answer
-
+  		#render json: {answer: 'мглою'}
+  		render json: {answer: @ans.to_s}
 		
   	end
 
